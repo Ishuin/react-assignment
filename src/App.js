8863e07a5d9e57cf37ff6af146bdcb299b6244f2
Ishuin <ishu.kumars@gmail.com>
2019-09-22 11:37:34 +0530

    base syntax assignment complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 
 class App extends Component {
+  state = {
+    username: 'kumar'
+  }
+
+  usernameChangedHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
+        <UserInput
+          changed = {this.usernameChangedHandler}
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="xyz" />
+        <UserOutput userName="abc" />
       </div>
     );
   }
-}
+};
 
 export default App;
